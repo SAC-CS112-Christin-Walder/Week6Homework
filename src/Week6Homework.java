edc4f048dab6abf77f6a8cb18bf45b52bfde67ad
SAC-CS112-Christin-Walder <cdubs808@gmail.com>
2015-10-06 21:55:25 -0700

    add while statement

--- src/Week6Homework.java
+++ src/Week6Homework.java
@@ -8,42 +8,51 @@ public class Week6Homework
 		
 		int user = 0;
 		int computer;
+		String repeat;
 		
-		System.out.print("Rock, paper or scissors?: ");
-		String userInput=input.next();
-		
-		userInput = userInput.toUpperCase();
-		if (userInput.equals("ROCK") || userInput.equals("PAPER") || userInput.equals("SCISSORS"))
-		{
-			if (userInput.equals("ROCK"))
-				user = 1;
-			else if (userInput.equals("PAPER"))
-				user = 2; 
-			else if (userInput.equals("SCISSORS"))
-				user = 3;
+		do {
+			System.out.print("Rock, paper or scissors?: ");
+			String userInput=input.next();
+			
+			userInput = userInput.toUpperCase();
+			if (userInput.equals("ROCK") || userInput.equals("PAPER") || userInput.equals("SCISSORS"))
+			{
+				if (userInput.equals("ROCK"))
+					user = 1;
+				else if (userInput.equals("PAPER"))
+					user = 2; 
+				else if (userInput.equals("SCISSORS"))
+					user = 3;
+					
 				
+				computer = 1 + (int)(Math.random() * 3);
+					if (computer==1)
+						System.out.println("Computer: Rock");
+					else if (computer==2)
+						System.out.println("Computer: Paper");
+					else if (computer==3)
+						System.out.println("Computer: Scissors");
+				
+				if (user==computer)
+					System.out.println("It's a tie!");
+				else if ((user==1 && computer==2)||(user==2 && computer==1))
+					System.out.println("The computer wins!");
+				else if ((user==1 && computer==3)||(user==3 && computer==1))
+					System.out.println("You win!");
+				else if ((user==2 && computer==3)||(user==3 && computer==2))
+					System.out.println("The computer wins!");
+				//How to stop program and loop back around for invalid selection
+			}
+			else 
+				System.out.println("Invalid selection");
 			
-			computer = 1 + (int)(Math.random() * 3);
-				if (computer==1)
-					System.out.println("Computer: Rock");
-				else if (computer==2)
-					System.out.println("Computer: Paper");
-				else if (computer==3)
-					System.out.println("Computer: Scissors");
-			
-			if (user==computer)
-				System.out.println("It's a tie!");
-			else if ((user==1 && computer==2)||(user==2 && computer==1))
-				System.out.println("The computer wins!");
-			else if ((user==1 && computer==3)||(user==3 && computer==1))
-				System.out.println("You win!");
-			else if ((user==2 && computer==3)||(user==3 && computer==2))
-				System.out.println("The computer wins!");
-			//How to stop program and loop back around for invalid selection
+			System.out.printf("Do you want to play again? (yes or no): ");
+			repeat=input.next();
+			repeat = repeat.toUpperCase();
 		}
-		else 
-			System.out.println("Invalid selection");
-		
-	}
+		while (repeat.equals("YES")); 
+			if (repeat != ("YES"))
+				System.out.print("Game over");
+			}
 
 }
